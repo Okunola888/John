f01ef65f63d8ad5da06e51f4473d9e4dc0d41a4c
Okunola888 <100191372+Okunola888@users.noreply.github.com>
2022-02-25 08:17:13 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int First_Num = 4;
+            double First_Value = 4.0;
+            string First_Name = "Poly Ilaro";
+
+            Console.WriteLine("enter your Second NUmber");
+            int Second_Num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter Your Second Double");
+            double Second_Value = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Your Comment");
+            String Comment = Console.ReadLine();
+
+            Console.WriteLine(First_Num + Second_Num);
+            Console.WriteLine(First_Value + Second_Value);
+            Console.WriteLine(First_Name + " " + Comment);
+            Console.ReadLine();
+        }
+    }
+}
